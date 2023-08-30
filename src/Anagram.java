03a923470b48544afb1ecc79b173746065fbc37a
masego8 <30150526@nescol.ac.uk>
2023-08-30 12:22:31 +0100

    Added HelloWorld Program

---
+++ src/Anagram.java
@@ -0,0 +1,12 @@
+public class Anagram {
+    public static main (String[] args) {
+        System.out.println(
+                "Here are 5 anagrams:\n"
+                "SILENT = LISTEN\n"
+                "TRIANGLE = ALTERING\n"
+                "RESCUE = SECURE\n"
+                "ASTRONOMER = MOONSTARER\n"
+                "DESPERATION = A ROPE ENDS IT\n"
+        );
+    }
+}
---
+++ src/Haiku.java
@@ -0,0 +1,12 @@
+public class Haiku {
+
+    public static void main (String[] args) {
+        System.out.println (
+                "In the stillness, calm\n" +
+                "\n" +
+                "Nature's beauty surrounds us\n" +
+                "\n" +
+                "Peaceful harmony");
+
+    }
+}
