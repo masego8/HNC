d842303911c3710ca8f725a1582ef02736d38c77
masego8 <30150526@nescol.ac.uk>
2023-08-30 12:03:02 +0100

    Added HelloWorld Program

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * My First Program
+ * @author Masego
+ * @version 1
+ */
+public class HelloWorld {
+    public static void main (String[] args) {
+        //write out hello world
+        System.out.println("Hello World");
+    }
+}
