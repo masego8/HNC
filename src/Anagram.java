5c874cf549ab72735572b6e4002030af5a13d03b
masego8 <30150526@nescol.ac.uk>
2023-08-30 18:31:44 +0100

    downloading github on my personal laptop

--- src/Anagram.java
+++ src/Anagram.java
@@ -1,11 +1,11 @@
 public class Anagram {
-    public static main (String[] args) {
+    public static void main(String[] args) {
         System.out.println(
-                "Here are 5 anagrams:\n"
-                "SILENT = LISTEN\n"
-                "TRIANGLE = ALTERING\n"
-                "RESCUE = SECURE\n"
-                "ASTRONOMER = MOONSTARER\n"
+                "Here are 5 anagrams:\n" +
+                "SILENT = LISTEN\n" +
+                "TRIANGLE = ALTERING\n" +
+                "RESCUE = SECURE\n" +
+                "ASTRONOMER = MOONSTARER\n" +
                 "DESPERATION = A ROPE ENDS IT\n"
         );
     }
