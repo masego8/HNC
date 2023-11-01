cba2a25399047041a0552ba8638dd8718be3b4b4
masego8 <30150526@nescol.ac.uk>
2023-11-01 12:03:39 +0000

    Exercises from Week 7

---
+++ src/Week_8/L8E1.java
@@ -0,0 +1,11 @@
+package Week_8;
+
+public class L8E1 {
+
+    public static void main(String[] args) {
+        String[] students = {"Matthew", "Bill", "Otormiefe","Oliver", "Dante"};
+        System.out.println(students[0]);
+        System.out.println(students[4]);
+        System.out.println(students.length);
+    }
+}
---
+++ src/Week_8/L8E2.java
@@ -0,0 +1,12 @@
+package Week_8;
+
+public class L8E2 {
+
+    public static void main(String[] args) {
+        int[] scores = {84,76,91,58,73};
+        for (int index = 0; index < scores.length; index++) {
+            System.out.println(scores[index]);
+
+        }
+    }
+}
