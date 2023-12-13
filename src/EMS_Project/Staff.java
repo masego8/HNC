abe7ac28628f9d04b9b78e75358fd999863be9b3
masego8 <30150526@nescol.ac.uk>
2023-12-13 21:28:57 +0000

    First Sprint of the Java Project.

---
+++ src/EMS_Project/Staff.java
@@ -0,0 +1,13 @@
+package EMS_Project;
+
+public class Staff {
+
+    public int employeeId = 0;
+    public String firstname = "";
+    public String surname = "";
+    public String role = "";
+    public String email = "";
+
+
+
+}
